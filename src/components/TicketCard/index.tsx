2797fcb20783a6a8cdf368689509e44dcee3ca22
Akash Khaitan <akashkhaitan@gmail.com>
2024-09-19 16:38:50 +0530

    feat: updated TicketCard

--- src/components/TicketCard/index.tsx
+++ src/components/TicketCard/index.tsx
@@ -5,18 +5,25 @@ interface TicketCardProps {
   title: string;
   description: string;
   status: "todo" | "in-progress" | "done";
+  priority: number;
 }
 
 const TicketCard: React.FC<TicketCardProps> = ({
   title,
   description,
   status,
+  priority,
 }) => {
   return (
     <div className={`ticket-card`}>
+      <div className="card-heading">
+        <span>{status}</span>
+        <span>sd</span>
+      </div>
       <h3>{title}</h3>
-      <p>{description}</p>
-      <p>Status: {status}</p>
+
+      {/* Tags Section */}
+      <div className="tags"></div>
     </div>
   );
 };
